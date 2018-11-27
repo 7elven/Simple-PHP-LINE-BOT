2e79d0b9b1caf5343e47e6b5bb59459d5e4d625b
Nontachai Korninai <dezember2012@gmail.com>
2018-11-27 12:23:36 +0700

    Update setting.php
    Fix Warning
    In PHP 7, calling non-static methods statically is deprecated, and will generate an E_DEPRECATED warning. Support for calling non-static methods statically may be removed in the future.

--- setting.php
+++ setting.php
@@ -1,19 +1,19 @@
 <?php
 
 class Setting {
-	public function getChannelAccessToken(){
+	public static function getChannelAccessToken(){
 		$channelAccessToken = "";
 		return $channelAccessToken;
 	}
-	public function getChannelSecret(){
+	public static function getChannelSecret(){
 		$channelSecret = "";
 		return $channelSecret;
 	}
-	public function getApiReply(){
+	public static function getApiReply(){
 		$api = "https://api.line.me/v2/bot/message/reply";
 		return $api;
 	}
-	public function getApiPush(){
+	public static function getApiPush(){
 		$api = "https://api.line.me/v2/bot/message/push";
 		return $api;
 	}
